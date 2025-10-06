caa907e32f7541cb7e62b986e1c24d87dc7a78a1
nika <veronik.burkova@gmail.com>
2025-10-06 22:20:44 +0300

    class for printing results

---
+++ result_printer.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <ostream>
+#include "distribution_analysis.h"
+#include <unordered_map>
+#include <format>
+
+class ResultPrinter {
+private:
+    std::ostream& stream;
+public:
+    ResultPrinter(std::ostream& stream) : stream(stream) {}
+
+    void print(const std::string& result) const {
+        stream << result << std::endl;
+    }
+
+    void print(const std::vector<int>& values, const DistributionAnalysis::AnalysisResult& result) const {
+        print("Input numbers: ");
+        for (const int &n: values)
+            stream << n << " ";
+
+        stream << std::endl;
+
+        print("Input frequencies: ");
+        for (const int &n: values)
+            stream << result.expected_frequencies.at(n) << " ";
+        stream << std::endl;
+
+        print("Real frequencies: ");
+        for (const int &n: values)
+            stream << result.real_frequencies.at(n) << " ";
+        stream << std::endl;
+
+        print(std::format("Max frequencies deviation: {:.4f}", result.max_deviation));
+
+    }
+
+};
